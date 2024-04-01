17db215ffd254ee0d5bf4257420871759a0aaece
KawshikBss <kawshikbss@gmail.com>
2024-04-02 00:23:35 +0600

    created placeholder data

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,64 @@
+export const categories = [
+    {
+        name: "Size",
+        items: [
+            { name: "Small", slug: "small" },
+            { name: "Medium", slug: "medium" },
+            { name: "Large", slug: "large" },
+            { name: "Extra Large", slug: "extra-large" },
+            { name: "XXL", slug: "xxl" },
+            { name: "XXXL", slug: "xxxl" },
+        ],
+    },
+    {
+        name: "Color",
+        items: [
+            { name: "Red", slug: "red" },
+            { name: "Blue", slug: "blue" },
+            { name: "Green", slug: "green" },
+            { name: "Black", slug: "black" },
+            { name: "White", slug: "white" },
+            { name: "Yellow", slug: "yellow" },
+        ],
+    },
+    {
+        name: "Brand",
+        items: [
+            { name: "Nike", slug: "nike" },
+            { name: "Adidas", slug: "adidas" },
+            { name: "Puma", slug: "puma" },
+            { name: "Reebok", slug: "reebok" },
+            { name: "Under Armour", slug: "under-armour" },
+            { name: "New Balance", slug: "new-balance" },
+        ],
+    },
+    {
+        name: "Price Range",
+        items: [
+            { name: "Under $50", slug: "under-50" },
+            { name: "$50 - $100", slug: "50-100" },
+            { name: "$100 - $200", slug: "100-200" },
+            { name: "$200 - $500", slug: "200-500" },
+            { name: "$500 - $1000", slug: "500-1000" },
+            { name: "Over $1000", slug: "over-1000" },
+        ],
+    },
+    {
+        name: "Discount",
+        items: [
+            { name: "10% Off or More", slug: "10-off" },
+            { name: "20% Off or More", slug: "20-off" },
+            { name: "30% Off or More", slug: "30-off" },
+            { name: "40% Off or More", slug: "40-off" },
+            { name: "50% Off or More", slug: "50-off" },
+            { name: "60% Off or More", slug: "60-off" },
+        ],
+    },
+    {
+        name: "Availability",
+        items: [
+            { name: "In Stock", slug: "in-stock" },
+            { name: "Out of Stock", slug: "out-of-stock" },
+        ],
+    },
+];
