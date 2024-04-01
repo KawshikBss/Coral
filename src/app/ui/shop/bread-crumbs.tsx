a23f18aadcf568cf246c1c8d4c12255eaba60648
KawshikBss <kawshikbss@gmail.com>
2024-04-02 00:24:59 +0600

    created bread crumbs

---
+++ src/app/ui/shop/bread-crumbs.tsx
@@ -0,0 +1,37 @@
+import { BreadCrumb } from "@/app/lib/defenitions";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import React, { Fragment } from "react";
+
+type Props = {
+    breadCrumbs: BreadCrumb[];
+};
+
+function BreadCrumbs({ breadCrumbs }: Props) {
+    return (
+        <ul className="w-full px-5 flex flex-row justify-start items-center gap-1">
+            {breadCrumbs.map((item, index) => (
+                <Fragment key={index}>
+                    <li
+                        className={clsx(
+                            index < breadCrumbs.length - 1
+                                ? "text-[#1B4B66]"
+                                : "text-[#626262]",
+                            "text-base font-medium"
+                        )}
+                    >
+                        <Link href={item.href}>{item.name}</Link>
+                    </li>
+                    {breadCrumbs.length - 1 > index ? (
+                        <ChevronRightIcon width={24} height={24} />
+                    ) : (
+                        ""
+                    )}
+                </Fragment>
+            ))}
+        </ul>
+    );
+}
+
+export default BreadCrumbs;
