a67fac7ad39dc3249cb49fbaacba6988bfe7c40f
KawshikBss <kawshikbss@gmail.com>
2024-04-04 03:13:14 +0600

    added type for product

--- src/app/lib/defenitions.ts
+++ src/app/lib/defenitions.ts
@@ -7,3 +7,28 @@ export type CategoryItem = {
     name: string;
     slug: string;
 };
+
+export type Product = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    brand: string;
+    category: string;
+    color: string[];
+    size: string[];
+    images: string[];
+    thumbnail: string;
+    stockAvailability: string;
+    discount?: string;
+    rating?: number;
+    slug: string;
+    sku: string;
+    weight?: number;
+    dimensions?: {
+        length: number;
+        width: number;
+        height: number;
+    };
+    tags: string[];
+};
