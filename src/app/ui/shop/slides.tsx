600c93f642e8fc292bb17191a7f538d0dad380e8
KawshikBss <kawshikbss@gmail.com>
2024-04-07 00:28:44 +0600

    created slide for product images

---
+++ src/app/ui/shop/slides.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Product } from "@/app/lib/defenitions";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import React, { useState } from "react";
+
+type Props = { product: Product };
+
+function Slides({ product }: Props) {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const handlePrevSlide = () => {
+        setCurrentSlide((prev) => {
+            return prev - 1 >= 0 ? prev - 1 : product.images.length - 1;
+        });
+    };
+    const handleNextSlide = () => {
+        setCurrentSlide((prev) => {
+            return prev + 1 < product.images.length ? prev + 1 : 0;
+        });
+    };
+    return (
+        <div className="w-full flex flex-col justify-between items-center">
+            <Image
+                src={product.images[currentSlide]}
+                alt={product.name}
+                width={605}
+                height={605}
+                className="object-cover object-center rounded-2xl"
+            />
+            <div className="w-full mx-16 mt-6 flex justify-center items-center gap-8">
+                <ChevronLeftIcon
+                    width={24}
+                    height={24}
+                    onClick={handlePrevSlide}
+                />
+                <div className="w-full">
+                    <div className="flex justify-center items-center gap-8">
+                        {product.images.map((item, index) => (
+                            <Image
+                                src={item}
+                                alt={item}
+                                key={index}
+                                width={75}
+                                height={75}
+                                className="object-cover object-center rounded-xl cursor-pointer"
+                                onClick={() => setCurrentSlide(index)}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <ChevronRightIcon
+                    width={24}
+                    height={24}
+                    onClick={handleNextSlide}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Slides;
