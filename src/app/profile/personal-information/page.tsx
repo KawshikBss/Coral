cd901027fe2963bff5694f42edc56088f2c2d9f5
KawshikBss <kawshikbss@gmail.com>
2024-04-09 14:39:57 +0600

    moved profile edit to new page

---
+++ src/app/profile/personal-information/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumbs from "../../ui/shop/bread-crumbs";
+import Link from "next/link";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import SidePanel from "../../ui/profile/side-panel";
+import ProfileEditForm from "../../ui/profile/profile-edit-form";
+
+function Profile() {
+    return <ProfileEditForm />;
+}
+
+export default Profile;
