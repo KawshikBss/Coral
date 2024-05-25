cc1af81e968fa63f48ba9192bb32b0273707fd40
KawshikBss <kawshikbss@gmail.com>
2024-05-25 20:07:00 +0600

    fixed filter functionality

--- src/app/ui/shop/product-catalog.tsx
+++ src/app/ui/shop/product-catalog.tsx
@@ -1,20 +1,43 @@
 "use client";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import FilterPanel from "./filter-panel";
 import Card from "./product-card";
 import { FunnelIcon } from "@heroicons/react/24/outline";
 import FilterModal from "./filter-modal";
-import { products } from "@/app/lib/placeholder-data";
+import { Brand, Color, Product } from "@/app/lib/defenitions";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-type Props = {};
+type Props = {
+    productsList: Product[] | [];
+    filterLists: { brandList: Brand[]; colorList: Color[] };
+};
 
-function ProductCatalog({}: Props) {
+function ProductCatalog({ productsList, filterLists }: Props) {
     const [showFilterModal, setShowFilterModal] = useState(false);
     const toggleFilterModal = () => setShowFilterModal((prev) => !prev);
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const { replace } = useRouter();
+    const handleProductLimitChange = (
+        event: ChangeEvent<HTMLSelectElement>
+    ) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("limit", event.target.value);
+        replace(`${pathname}?${params.toString()}`);
+    };
+    const handleProductSortChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("sort_by", event.target.value);
+        replace(`${pathname}?${params.toString()}`);
+    };
     return (
         <div className="w-full flex flex-row justify-between items-start">
-            <FilterPanel />
-            <FilterModal show={showFilterModal} toggle={toggleFilterModal} />
+            <FilterPanel filterLists={filterLists} />
+            <FilterModal
+                show={showFilterModal}
+                toggle={toggleFilterModal}
+                filterLists={filterLists}
+            />
             <div className="w-full md:w-4/5 flex flex-col justify-between p-4 md:px-8">
                 <div className="w-full flex flex-col md:flex-row justify-between items-start md:items-center gap-2 md:gap-0 pb-6">
                     <span className="w-full md:w-fit flex flex-row justify-between items-center">
@@ -28,7 +51,12 @@ function ProductCatalog({}: Props) {
                     </span>
                     <span>
                         To Show:{" "}
-                        <select className="bg-[#F1F1F1] px-1 md:px-2 py-2 md:py-4 rounded-lg">
+                        <select
+                            className="bg-[#F1F1F1] px-1 md:px-2 py-2 md:py-4 rounded-lg"
+                            onChange={(event) =>
+                                handleProductLimitChange(event)
+                            }
+                        >
                             <option value="20">20</option>
                             <option value="30">30</option>
                             <option value="40">40</option>
@@ -36,19 +64,18 @@ function ProductCatalog({}: Props) {
                     </span>
                     <span>
                         Sort By:{" "}
-                        <select className="bg-[#F1F1F1] px-1 md:px-2 py-2 md:py-4 rounded-lg">
-                            <option value="Rating">Rating</option>
-                            <option value="Price (Low to High)">
-                                Price (Low to High)
-                            </option>
-                            <option value="Price (High to Low)">
-                                Price (High to Low)
-                            </option>
+                        <select
+                            className="bg-[#F1F1F1] px-1 md:px-2 py-2 md:py-4 rounded-lg"
+                            onChange={(event) => handleProductSortChange(event)}
+                        >
+                            <option value="">Rating</option>
+                            <option value="ASC">Price (Low to High)</option>
+                            <option value="DESC">Price (High to Low)</option>
                         </select>
                     </span>
                 </div>
                 <div className="w-full flex flex-wrap justify-center items-start gap-8 gap-y-8 md:gap-y-16 mb-6">
-                    {products.map((item, index) => (
+                    {productsList.map((item, index) => (
                         <Card product={item} key={index} />
                     ))}
                 </div>
