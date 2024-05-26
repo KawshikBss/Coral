cfa66834ce45eff175f74c985b9eda63f9db65ea
KawshikBss <kawshikbss@gmail.com>
2024-05-26 14:04:43 +0600

    fetched products from db

--- src/app/profile/my-wishlist/page.tsx
+++ src/app/profile/my-wishlist/page.tsx
@@ -1,10 +1,11 @@
-import { products } from "@/app/lib/placeholder-data";
+import { fetchProducts } from "@/app/lib/data/products";
 import Card from "@/app/ui/shop/product-card";
 import React from "react";
 
 type Props = {};
 
-function MyWishlist({}: Props) {
+async function MyWishlist({}: Props) {
+    const products = await fetchProducts();
     return (
         <div className="w-full md:w-4/5 flex flex-wrap justify-center items-start gap-8 gap-y-8 md:gap-y-16">
             {products.map((item, index) => (
